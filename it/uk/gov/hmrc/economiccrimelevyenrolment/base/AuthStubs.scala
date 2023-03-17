e0ca89f8bddeb51a4a0880451c9c028ca308bb2b
TonyCOgilvie <111900771+TonyCOgilvie@users.noreply.github.com>
2023-03-17 15:19:43 +0000

    Refactored integration tests

---
+++ it/uk/gov/hmrc/economiccrimelevyenrolment/base/AuthStubs.scala
@@ -0,0 +1,152 @@
+package uk.gov.hmrc.economiccrimelevyenrolment.base
+
+import com.github.tomakehurst.wiremock.client.WireMock._
+import com.github.tomakehurst.wiremock.stubbing.StubMapping
+import play.api.http.Status.{OK, UNAUTHORIZED}
+import play.api.test.Helpers.WWW_AUTHENTICATE
+import uk.gov.hmrc.economiccrimelevyenrolment.base.WireMockHelper.stub
+import uk.gov.hmrc.economiccrimelevyenrolment.models.eacd.EclEnrolment
+
+trait AuthStubs { self: WireMockStubs =>
+
+  def stubAuthorised(): StubMapping =
+    stub(
+      post(urlEqualTo("/auth/authorise"))
+        .withRequestBody(
+          equalToJson(
+            s"""
+               |{
+               |  "authorise": [],
+               |  "retrieve": [ "internalId", "allEnrolments", "groupIdentifier", "affinityGroup", "credentialRole" ]
+               |}
+           """.stripMargin,
+            true,
+            true
+          )
+        ),
+      aResponse()
+        .withStatus(OK)
+        .withBody(s"""
+             |{
+             |  "internalId": "$testInternalId",
+             |  "groupIdentifier": "$testGroupId",
+             |  "affinityGroup": "Organisation",
+             |  "credentialRole": "User",
+             |  "allEnrolments": []
+             |}
+           """.stripMargin)
+    )
+
+  def stubUnauthorised(): StubMapping =
+    stub(
+      post(urlEqualTo("/auth/authorise"))
+        .withRequestBody(
+          equalToJson(
+            s"""
+               |{
+               |  "authorise": [],
+               |  "retrieve": [ "internalId", "allEnrolments", "groupIdentifier", "affinityGroup", "credentialRole" ]
+               |}
+           """.stripMargin,
+            true,
+            true
+          )
+        ),
+      aResponse()
+        .withStatus(UNAUTHORIZED)
+        .withHeader(WWW_AUTHENTICATE, "MDTP detail=\"MissingBearerToken\"")
+    )
+
+  def stubAuthorisedWithAgentAffinityGroup(): StubMapping =
+    stub(
+      post(urlEqualTo("/auth/authorise"))
+        .withRequestBody(
+          equalToJson(
+            s"""
+               |{
+               |  "authorise": [],
+               |  "retrieve": [ "internalId", "allEnrolments", "groupIdentifier", "affinityGroup", "credentialRole" ]
+               |}
+           """.stripMargin,
+            true,
+            true
+          )
+        ),
+      aResponse()
+        .withStatus(OK)
+        .withBody(s"""
+             |{
+             |  "internalId": "$testInternalId",
+             |  "groupIdentifier": "$testGroupId",
+             |  "affinityGroup": "Agent",
+             |  "credentialRole": "User",
+             |  "allEnrolments": []
+             |}
+           """.stripMargin)
+    )
+
+  def stubAuthorisedWithEclEnrolment(): StubMapping =
+    stub(
+      post(urlEqualTo("/auth/authorise"))
+        .withRequestBody(
+          equalToJson(
+            s"""
+               |{
+               |  "authorise": [],
+               |  "retrieve": [ "internalId", "allEnrolments", "groupIdentifier", "affinityGroup", "credentialRole" ]
+               |}
+           """.stripMargin,
+            true,
+            true
+          )
+        ),
+      aResponse()
+        .withStatus(OK)
+        .withBody(s"""
+             |{
+             |  "internalId": "$testInternalId",
+             |  "groupIdentifier": "$testGroupId",
+             |  "affinityGroup": "Organisation",
+             |  "credentialRole": "User",
+             |  "allEnrolments": [{
+             |    "key":"${EclEnrolment.ServiceName}",
+             |    "identifiers": [{ "key":"${EclEnrolment.IdentifierKey}", "value": "$testEclRegistrationReference" }],
+             |    "state": "activated"
+             |  }]
+             |}
+           """.stripMargin)
+    )
+
+  def stubAuthorisedWithAssistantCredentialRole(): StubMapping =
+    stub(
+      post(urlEqualTo("/auth/authorise"))
+        .withRequestBody(
+          equalToJson(
+            s"""
+               |{
+               |  "authorise": [],
+               |  "retrieve": [ "internalId", "allEnrolments", "groupIdentifier", "affinityGroup", "credentialRole" ]
+               |}
+         """.stripMargin,
+            true,
+            true
+          )
+        ),
+      aResponse()
+        .withStatus(OK)
+        .withBody(s"""
+             |{
+             |  "internalId": "$testInternalId",
+             |  "groupIdentifier": "$testGroupId",
+             |  "affinityGroup": "Organisation",
+             |  "credentialRole": "Assistant",
+             |  "allEnrolments": [{
+             |    "key":"${EclEnrolment.ServiceName}",
+             |    "identifiers": [{ "key":"${EclEnrolment.IdentifierKey}", "value": "$testEclRegistrationReference" }],
+             |    "state": "activated"
+             |  }]
+             |}
+         """.stripMargin)
+    )
+
+}
