7c1c533a53f9866a42b5306186cfd51f62498024
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2023-06-27 14:40:00 +0100

    ECL-453: Updated dependencies and PR bot configuration changes (#19)
    * ECL-453: Updated Dependencies
    
    * ECL-453: PR bot configuration changes
    
    * ECL-453: PR bot configuration change

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "7.15.0"
-  private val hmrcMongoVersion     = "1.1.0"
+  private val hmrcBootstrapVersion = "7.19.0"
+  private val hmrcMongoVersion     = "1.3.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.3.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.13.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.13.0-play-28"
   )
 
