00d2b4f456b46dd8aa9989a9b719b76ff1ff0004
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2024-02-29 16:00:32 +0000

    ECL-717: Play 3.0 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,22 +2,22 @@ import sbt._
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "7.22.0"
-  private val hmrcMongoVersion     = "1.3.0"
+  private val hmrcBootstrapVersion = "8.4.0"
+  private val hmrcMongoVersion     = "1.7.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % hmrcBootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-28"    % "8.5.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.13.0-play-28"
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % hmrcBootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "8.5.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "2.0.0"
   )
 
   val test: Seq[ModuleID]    = Seq(
-    "uk.gov.hmrc"          %% "bootstrap-test-play-28"   % hmrcBootstrapVersion,
-    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-28"  % hmrcMongoVersion,
-    "org.jsoup"             % "jsoup"                    % "1.16.1",
-    "org.mockito"          %% "mockito-scala"            % "1.17.27",
-    "org.scalatestplus"    %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
+    "uk.gov.hmrc"          %% "bootstrap-test-play-30"   % hmrcBootstrapVersion,
+    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30"  % hmrcMongoVersion,
+    "org.jsoup"             % "jsoup"                    % "1.17.2",
+    "org.mockito"          %% "mockito-scala"            % "1.17.30",
+    "org.scalatestplus"    %% "scalacheck-1-17"          % "3.2.18.0",
     "com.danielasfregola"  %% "random-data-generator"    % "2.9",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"    % "1.1.0"
   ).map(_ % "test, it")
