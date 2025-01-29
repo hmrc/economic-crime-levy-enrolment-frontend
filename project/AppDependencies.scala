5f3c8aae624f7371fde70c57afb8a8f7a4545f95
paweldigital <173694350+paweldigital@users.noreply.github.com>
2025-01-29 15:03:24 +0000

    DDCYLS-5994: initial dependency updates
    Signed-off-by: paweldigital <173694350+paweldigital@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,21 +2,21 @@ import sbt._
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "8.5.0"
-  private val hmrcMongoVersion     = "1.8.0"
+  private val hmrcBootstrapVersion = "9.7.0"
+  private val hmrcMongoVersion     = "2.4.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "9.3.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "2.0.0"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "11.11.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.2.0"
   )
 
   val test: Seq[ModuleID]    = Seq(
     "uk.gov.hmrc"          %% "bootstrap-test-play-30"   % hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30"  % hmrcMongoVersion,
-    "org.jsoup"             % "jsoup"                    % "1.17.2",
-    "org.mockito"          %% "mockito-scala"            % "1.17.30",
+    "org.jsoup"             % "jsoup"                    % "1.18.3",
+    "org.mockito"          %% "mockito-scala"            % "1.17.37",
     "org.scalatestplus"    %% "scalacheck-1-17"          % "3.2.18.0",
     "com.danielasfregola"  %% "random-data-generator"    % "2.9",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"    % "1.1.0"
