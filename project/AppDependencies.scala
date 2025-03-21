6a318f5c6e35014d8c668f86e2995a5ca30c4907
Vic Watson <171040641+VJWatson@users.noreply.github.com>
2025-03-21 15:27:00 +0000

    DDCYLS-6434: app dep updates (#52)
    * app dep updates
    
    * removed logger

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,14 +1,14 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "9.7.0"
-  private val hmrcMongoVersion     = "2.4.0"
+  private val hmrcBootstrapVersion = "9.11.0"
+  private val hmrcMongoVersion     = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "11.11.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.0.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.2.0"
   )
 
