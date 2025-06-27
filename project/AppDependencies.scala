8b6649e7c417920ff7912cfa826117cea0d38ade
VJWatson <171040641+VJWatson@users.noreply.github.com>
2025-06-27 14:31:01 +0100

    app deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,21 +2,21 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "9.11.0"
+  private val hmrcBootstrapVersion = "9.13.0"
   private val hmrcMongoVersion     = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.1.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.6.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0"
   )
 
   val test: Seq[ModuleID]    = Seq(
     "uk.gov.hmrc"          %% "bootstrap-test-play-30"   % hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30"  % hmrcMongoVersion,
-    "org.jsoup"             % "jsoup"                    % "1.18.3",
-    "org.mockito"          %% "mockito-scala"            % "1.17.37",
+    "org.jsoup"             % "jsoup"                    % "1.21.1",
+    "org.mockito"          %% "mockito-scala"            % "2.0.0",
     "org.scalatestplus"    %% "scalacheck-1-17"          % "3.2.18.0",
     "com.danielasfregola"  %% "random-data-generator"    % "2.9",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"    % "1.1.0"
