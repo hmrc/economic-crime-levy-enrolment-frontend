22cc552b21d8b303bde52e28f00eefe3ecb482ac
bharti.verma <68339135+bharti5verma@users.noreply.github.com>
2026-03-13 16:36:23 +0000

    DDCYLS-8214 Vulnerabilities fix

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,17 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "10.5.0"
+  private val hmrcBootstrapVersion = "10.7.0"
   private val hmrcMongoVersion     = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.31.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.4.0"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.32.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.4.0",
+    "ch.qos.logback"     % "logback-classic"                       % "1.5.27",
+    "org.apache.commons" % "commons-lang3"                         % "3.18.0",
+    "at.yawk.lz4"        % "lz4-java"                              % "1.10.3"
   )
 
   val test: Seq[ModuleID]    = Seq(
