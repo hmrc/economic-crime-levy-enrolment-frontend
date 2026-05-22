f8434c0e2b5a6615d77d76863ad3332fd4b7f280
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2026-05-22 15:07:35 +0100

    updating dependency and adding new nav

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.32.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.4.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "ch.qos.logback"     % "logback-classic"                       % "1.5.27",
     "org.apache.commons" % "commons-lang3"                         % "3.18.0"
