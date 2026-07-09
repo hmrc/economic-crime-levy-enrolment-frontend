d931f173702490a6d33efb197dfcc3928338736e
AbdulHQuadri <67271481+AbdulHQuadri@users.noreply.github.com>
2026-07-09 15:39:57 +0100

    govukfrontend upgrades (#64)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "10.7.0"
+  private val hmrcBootstrapVersion = "10.8.0"
   private val hmrcMongoVersion     = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.4.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.9.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "ch.qos.logback"     % "logback-classic"                       % "1.5.27",
     "org.apache.commons" % "commons-lang3"                         % "3.18.0"
